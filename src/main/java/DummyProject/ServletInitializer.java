119d476f164c37706ab973cc280d3a88be986298
Ubuntu <alidia87gmail@alidia87gmail.pxtw4m5lnhgu1bnldftrfiau5b.gx.internal.cloudapp.net>
2021-04-14 21:52:28 +0000

    Maven Dummy Project

---
+++ src/main/java/DummyProject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package DummyProject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DummyProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/DummyProject/DummyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package DummyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
